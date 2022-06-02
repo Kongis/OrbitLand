f285a6250f48523f2582569181250d22c4b36c60
Kongis <kongis.dev@protonmail.com>
2022-06-02 21:02:50 +0200

    .

--- Assets/Characters/Character Generator/tool/bin/CharGenerator.java
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-JLINK_VM_OPTIONS=
-DIR=`dirname $0`
-$DIR/java $JLINK_VM_OPTIONS -m mario.main/com.almasb.fxglgames.mario.chargen.CharGenerator "$@"
--- Assets/Characters/Character Generator/tool/bin/CharGenerator.java.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set JLINK_VM_OPTIONS=
-set DIR=%~dp0
-"%DIR%\java" %JLINK_VM_OPTIONS% -m mario.main/com.almasb.fxglgames.mario.chargen.CharGenerator %*
